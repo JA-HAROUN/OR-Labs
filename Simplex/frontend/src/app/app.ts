bc85ae7a311d026d1562246280eef74246e13e7d
MohamedElshewy <abdalluhmohamed@gmail.com>
2026-04-27 22:49:20 +0300

    feat: update tableau display with pivot highlighting and new styling

--- Simplex/frontend/src/app/app.ts
+++ Simplex/frontend/src/app/app.ts
@@ -16,14 +16,26 @@ export class AppComponent {
   onSolve(problemData: any) {
     console.log("Data received from input form:", problemData);
 
-    // TEMPORARY DUMMY DATA TO TEST THE UI
+    // TEMPORARY DUMMY DATA
     this.finalResults = {
       status: "Optimal Solution Found",
-      optimalValue: 12.5,
-      finalVariables: { x1: 2.5, x2: 0, s1: 0, s2: 4 },
+      optimalValue: 9,
+      finalVariables: { x1: 0, x2: 0, x3: 6, x4: 4, x5: 0, x6: 12 },
+      headers: ["x1", "x2", "x3", "x4", "x5", "x6", "RHS"],
       steps: [
-        { iteration: 1, tableau: [[1, -3, -5, 0, 0, 0], [0, 1, 0, 1, 0, 4], [0, 0, 2, 0, 1, 12]] },
-        { iteration: 2, tableau: [[1, -3, 0, 0, 2.5, 30], [0, 1, 0, 1, 0, 4], [0, 0, 1, 0, 0.5, 6]] }
+        {
+          iteration: 1,
+          basicVariables: ["x4", "x3", "x6", "-z"],
+          // NEW: Simulate Python telling us where the pivot is!
+          pivotRow: 1, // The 2nd row (index 1)
+          pivotCol: 0, // The 1st column (index 0)
+          tableau: [
+            [0, 1, 0, 1, -2, 0, 4],
+            [1, -2, 1, 0, -2, 0, 6],
+            [3, 2, 0, 0, -3, 1, 12],
+            [-6, 1, 0, 0, -1, 0, -9]
+          ]
+        }
       ]
     };
   }
